eb5fe0c5e84d101160685084c8389682fb9eea70
aditya-0212 <67250226+aditya-0212@users.noreply.github.com>
2024-11-28 20:24:34 +0530

    Update ContentView.swift

--- DemoProjectAdi/ContentView.swift
+++ DemoProjectAdi/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-           Text("Aditya")
+           Text("Aditya Choubisa is IOS developer at shipturtle")
         }
         .padding()
     }
