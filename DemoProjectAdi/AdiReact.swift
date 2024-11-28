682ab15733cce4b26d1a68c422c409a38cfcb1b0
aditya-0212 <67250226+aditya-0212@users.noreply.github.com>
2024-11-28 20:37:02 +0530

    Create AdiReact.swift

---
+++ DemoProjectAdi/AdiReact.swift
@@ -0,0 +1,16 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+           Text("Aditya Choubisa was React developer in 2023")
+            Text("He was exception React developer")
+            Text("He was then  25 years old")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
