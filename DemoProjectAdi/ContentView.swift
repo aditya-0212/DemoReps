1632fa8c1c58a03efebb8cb9ec3bd21f466bf3e3
aditya choubisa <adityachoubisa02@gmail.com>
2024-11-28 20:30:55 +0530

    about biodata

--- DemoProjectAdi/ContentView.swift
+++ DemoProjectAdi/ContentView.swift
@@ -11,6 +11,7 @@ struct ContentView: View {
     var body: some View {
         VStack {
            Text("Aditya Choubisa is IOS developer at shipturtle")
+            Text("He is exception developer")
         }
         .padding()
     }
