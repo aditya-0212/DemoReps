71714305f5004cabc09eabc42f2cbafbe143891c
aditya choubisa <adityachoubisa02@gmail.com>
2024-11-28 19:33:41 +0530

    Initial Commit

---
+++ DemoProjectAdi/DemoProjectAdiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoProjectAdiApp.swift
+//  DemoProjectAdi
+//
+//  Created by APPLE on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoProjectAdiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
